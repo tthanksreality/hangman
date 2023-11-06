34b7bd4ea4fd1f39f417c66de6a21489396d2847
tthanksreality <gymfreak500@abv.bg>
2023-11-06 14:38:55 +0200

    finished program

--- lib/main_program.rb
+++ lib/main_program.rb
@@ -34,7 +34,36 @@ class Guess
   def show_hidden_word
     @guessed_word
   end
+
+  def check_win
+    if @guessed_word.include?('*')
+      puts "You are out of turns! The word was #{@board}."
+    else
+      puts "You guessed the word! It was: #{@guessed_word}!"
+    end
+  end
+
+  def play(secret_word)
+    turns_remaining = 8
+    while turns_remaining.positive? && @guessed_word.include?('*')
+      puts "Word: #{@guessed_word}"
+      print "Turns remaining: #{turns_remaining}. Please enter your guess:"
+      guess = gets.chomp.downcase
+      if secret_word == guess
+        puts "Congrats, you got the word! It was #{@board}" 
+        return
+      end
+      if secret_word.include?(guess)
+        secret_word.chars.each_with_index do |char, index|
+          @guessed_word[index] = char if char == guess
+        end
+      else
+        turns_remaining -= 1
+      end
+    end
+    check_win
+  end
 end
 
-murtveca = Guess.new
-p murtveca.show_hidden_word
+game = Guess.new
+game.play(game.show_word)
